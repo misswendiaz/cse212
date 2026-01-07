0ec79b27ae0c76933de8cde711d2f728eb7b6303
misswendiaz <ldiaz@byupathway.edu>
2026-01-07 20:08:37 +0800

    completed Problem 3: Predict, Measure, and Compare Code - Algorithms

--- week01/teach/Program.cs
+++ week01/teach/Program.cs
@@ -1,12 +1,12 @@
 ﻿// Console.WriteLine("\n======================\nDivisors\n======================");
 // Divisors.Run();
 
-Console.WriteLine("\n======================\nArray Selector\n======================");
-ArraySelector.Run();
+// Console.WriteLine("\n======================\nArray Selector\n======================");
+// ArraySelector.Run();
 
 // Uncomment when you are ready to run this
-// Console.WriteLine("\n======================\nAlgorithms\n======================");
-// Algorithms.Run();
+Console.WriteLine("\n======================\nAlgorithms\n======================");
+Algorithms.Run();
 
 
 
@@ -14,5 +14,5 @@ ArraySelector.Run();
 // Console.WriteLine("\n======================\nDivisors Solution\n======================");
 // DivisorsSolution.Run();
 //
-Console.WriteLine("\n======================\nArray Selector Solution\n======================");
-ArraySelectorSolution.Run();
+// Console.WriteLine("\n======================\nArray Selector Solution\n======================");
+// ArraySelectorSolution.Run();
