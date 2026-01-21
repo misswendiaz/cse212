77c2ca75d22a3d016d3502518e0b20bc9f7ff4ae
misswendiaz <ldiaz@byupathway.edu>
2026-01-21 17:25:24 +0800

    Completed W03 Learn

--- week03/learn/DuplicateCounter.cs
+++ week03/learn/DuplicateCounter.cs
@@ -1,4 +1,6 @@
-﻿public class DuplicateCounter
+﻿using System.Linq.Expressions;
+
+public class DuplicateCounter
 {
     //Count how many duplicates are in a collection of data.
 
@@ -24,7 +26,20 @@
 
     private static int CountDuplicates(int[] data)
     {
-        // Add code here.
-        return 0;
+        var set = new HashSet<int>();
+        var duplicates = 0;
+
+        foreach (int element in data)
+        {
+            if (set.Contains(element))
+            {
+                duplicates++;
+            }
+            else
+            {
+                set.Add(element);
+            }
+        }
+        return duplicates;
     }
 }
--- week03/learn/Program.cs
+++ week03/learn/Program.cs
@@ -4,8 +4,10 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("\n======================\nDuplicate Counter\n======================");
-        DuplicateCounter.Run();
+        Console.Clear();
+        
+        // Console.WriteLine("\n======================\nDuplicate Counter\n======================");
+        // DuplicateCounter.Run();
 
         Console.WriteLine("\n======================\nTranslator\n======================");
         Translator.Run();
